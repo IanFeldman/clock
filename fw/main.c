9a512caf73b2371ddea2602f74a2c70ffea9f7ea
IanFeldman <ianrfeldman@gmail.com>
2025-07-26 15:34:01 -0700

    Attempt to fix boilerplate

--- fw/main.c
+++ fw/main.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
+    LPC_GPIO_PORT->DIR0 = (1 << 17);
+    while (1)
+    {
+        LPC_GPIO_PORT->NOT0 = (1 << 17);  // Toggle P0_17
+        for (volatile int i = 0; i < 500000; i++); // Delay
+    }
+
     return 1;
 }
 
